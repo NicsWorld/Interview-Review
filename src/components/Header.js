b15abf941b883a19ef77dcd4024560d9001fc296
nic <nicwoods63@gmail.com>
2021-03-15 19:10:45 -0500

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { AppBar, Typography } from '@material-ui/core';
+
+export default function Header() {
+  return (
+    <AppBar>
+    <Typography variant="h6">
+      Interview Review
+    </Typography>
+    </AppBar>
+  );
+}
---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+// import { AppBar, Typography } from '@material-ui/core';
+
+export default function Hero() {
+  return (
+    <div className="hero-container">
+      <img src="/image.jpg" alt="image" />
+    </div>
+  );
+}
